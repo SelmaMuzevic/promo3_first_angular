a0674bf0059af661374874807d26f48ce6224a7d
selma <selma@gmail.com>
2017-09-19 10:15:53 +0200

    creation package pour angular

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,16 +1,28 @@
 import { Component } from "@angular/core";
 
 
+/**
+ * Les components sont en gros les controleurs de Angular
+ * Ce sont des classes qui seront liées à un template
+ * html et qui déclareront en leur sein les propriétés
+ * et méthodes accessibles au template en question.
+ */
 @Component({
 
-    /* le selector indique la balise html qui sera liee à ce component
-    Si angular voit un <non-app></non-app> dans html , il viendra y mettre le AppComponent */
+     /*Le sélecteur indique la balise html qui sera liée
+    à ce component. Si angular voit un <mon-app></mon-app>
+    dans le html, il viendra y mettre le AppComponent
+    */
     selector: 'mon-app',
 
-    template: '<h1>{{message}}</h1>'
+     /* Le template correspond à la vue, c'est là où on 
+    mettra le html du component (lorsqu'on utilise template
+    on ne peut faire que des template inline, pour avoir des
+    template dans un fichier externe il faudra utiliser 
+    templateUrl)
+    */
+    templateUrl: './app.component.html'
 })
-
-export class AppComponent{
-    private message:string = 'un truc dedans';
-     
+export class AppComponent {
+    private message:string = 'My first page Angular !';
 }
--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,4 +1,6 @@
-import {platformBrowser} from '@angular/platform-browser';
+import {platformBrowserDynamic} from 
+'@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 
-platformBrowser().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule);
+
